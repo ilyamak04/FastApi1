070a13f512b01db673f362d568187a8c8b73d9d3
makilya04 <makilya04@gmail.com>
2024-05-11 14:18:51 +0300

    second endpoint

--- traiding_app/main.py
+++ traiding_app/main.py
@@ -6,3 +6,15 @@ app = FastAPI()
 @app.get("/")
 def get_hello():
     return "Hello world!"
+
+
+fake_users = [
+    {"id": 1, "role": "admin", "name": "Bob"},
+    {"id": 2, "role": "investor", "name": "John"},
+    {"id": 3, "role": "trader", "name": "Matt"},
+]
+
+
+@app.get("/users/{user_id}")
+def get_user(user_id: int):
+    return [user for user in fake_users if user.get("id") == user_id]
